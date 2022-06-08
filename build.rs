b686102fb45618b3ce8e9bc6417f59a2bdd23689
Peter Keuter <peter@peterkeuter.nl>
2022-06-08 15:15:25 +0200

    fix: build

--- build.rs
+++ build.rs
@@ -637,29 +637,33 @@ fn link_to_libraries(statik: bool) {
         println!("cargo:rustc-link-lib=z");
     }
 
+    if env::var("CARGO_FEATURE_BUILD_LIB_FDK_AAC").is_ok() && cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-lib=fdk-aac");
+    }
+
     if env::var("CARGO_FEATURE_BUILD_LIB_NPP").is_ok() && cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-lib=dylib=nppc");
-        println!("cargo:rustc-link-lib=dylib=nppif");
-        println!("cargo:rustc-link-lib=dylib=nppig");
-        println!("cargo:rustc-link-lib=dylib=nppim");
-        println!("cargo:rustc-link-lib=dylib=nppist");
-        println!("cargo:rustc-link-lib=dylib=nppicc");
-        println!("cargo:rustc-link-lib=dylib=nppisu");
-        println!("cargo:rustc-link-lib=dylib=nppitc");
-        println!("cargo:rustc-link-lib=dylib=npps");
-        println!("cargo:rustc-link-lib=dylib=nppial");
-        println!("cargo:rustc-link-lib=dylib=nppidei");
-        println!("cargo:rustc-link-lib=dylib=npps");
+        println!("cargo:rustc-link-lib=nppc");
+        println!("cargo:rustc-link-lib=nppif");
+        println!("cargo:rustc-link-lib=nppig");
+        println!("cargo:rustc-link-lib=nppim");
+        println!("cargo:rustc-link-lib=nppist");
+        println!("cargo:rustc-link-lib=nppicc");
+        println!("cargo:rustc-link-lib=nppisu");
+        println!("cargo:rustc-link-lib=nppitc");
+        println!("cargo:rustc-link-lib=npps");
+        println!("cargo:rustc-link-lib=nppial");
+        println!("cargo:rustc-link-lib=nppidei");
+        println!("cargo:rustc-link-lib=npps");
     }
 
     if env::var("CARGO_FEATURE_BUILD_CUDA_NVCC").is_ok() && cfg!(target_os = "linux") {
         println!("cargo:rustc-link-lib=z");
         println!("cargo:rustc-link-lib=m");
-        println!("cargo:rustc-link-lib=dylib=va");
-        println!("cargo:rustc-link-lib=dylib=va-drm");
-        println!("cargo:rustc-link-lib=dylib=va-x11");
-        println!("cargo:rustc-link-lib=dylib=vdpau");
-        println!("cargo:rustc-link-lib=dylib=X11");
+        println!("cargo:rustc-link-lib=va");
+        println!("cargo:rustc-link-lib=va-drm");
+        println!("cargo:rustc-link-lib=va-x11");
+        println!("cargo:rustc-link-lib=vdpau");
+        println!("cargo:rustc-link-lib=X11");
     }
 }
 
